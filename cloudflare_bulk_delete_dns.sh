e2509ebd9bfc2625441e661f0e74c54549a08be2
Luana <47929717+luanacc@users.noreply.github.com>
2022-03-21 19:57:16 -0300

    Add files via upload

---
+++ cloudflare_bulk_delete_dns.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#TOKEN do domínio (obtenha em https://dash.cloudflare.com/profile/api-tokens):
+TOKEN=""
+
+#Informe aqui o ID da Zona:
+ZONE_ID=
+
+curl -s -X GET https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?per_page=500 \
+    -H "Authorization: Bearer ${TOKEN}" \
+    -H "Content-Type: application/json" | jq .result[].id |  tr -d '"' | (
+  while read id; do
+    curl -s -X DELETE https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${id} \
+      -H "Authorization: Bearer ${TOKEN}" \
+      -H "Content-Type: application/json"
+  done
+  )
